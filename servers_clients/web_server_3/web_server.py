1cdb101b1a5e6c452e52f1dd7619c79e50c17f77
root <root@(none)>
2020-05-01 05:04:19 -0500

    add web-server for return image

---
+++ servers_clients/web_server_3/web_server.py
@@ -0,0 +1,33 @@
+import socket
+
+'''
+В ответ на запрос web-сервер возвращает пользователю изображение.
+Запрос выполняется через браузер, адрес: http://localhost:8000
+'''
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('localhost', 8000))
+s.listen(1)
+
+try:
+    while True:
+        conn, addr = s.accept()
+        data = conn.recv(1024)
+
+        f = open("rocket.jpg", "rb")
+        content = f.read()
+        f.close()
+
+        response = '''HTTP/1.1 200 OK
+Content-Type: image/jpeg
+Connection: Closed
+
+'''.encode() + content
+
+        conn.send(response)
+        conn.close()
+
+except KeyboardInterrupt:
+    pass
+finally:
+    s.close()

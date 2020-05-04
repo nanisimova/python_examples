8bb948c4a044ae0e107996aae475effd42a207bb
root <root@(none)>
2020-05-04 13:24:09 -0500

    script for adding watermarks, add comment

--- graphics/watermarks/create_watermarks.py
+++ graphics/watermarks/create_watermarks.py
@@ -3,6 +3,7 @@ from PIL import Image
 img = Image.open("abstract.png")
 wmark = Image.open("logo.png")
 
+# Red, Green, Blue, Alpha
 marked = Image.new('RGBA', (300, 218), (0, 0, 0, 0)) # 300 - width img, 218 - height img
 marked.paste(img, (0, 0)) # (0, 0) - position
 marked.paste(wmark, (0, 0), mask=wmark)

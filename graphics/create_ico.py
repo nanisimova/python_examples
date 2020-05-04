544a4b38665259eb6dd8c5f960cc46cb2a58d278
root <root@(none)>
2020-05-04 12:05:47 -0500

    changed icon-save

--- graphics/create_ico.py
+++ graphics/create_ico.py
@@ -4,7 +4,7 @@ filename = 'filename1.png'
 try:
     fh = Image.open(filename)
     fh.thumbnail((16, 16))
-    fh.save('filename2.ico')
+    fh.save('filename2.ico', 'ico')
 
 except FileNotFoundError:
     print("File not found")
